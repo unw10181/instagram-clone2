3d4b7ed22e870494e3bf6d625a0b4afefc9b2544
unw10181 <uchennanwilliams@gmail.com>
2026-02-18 02:14:55 -0500

    allowing cloudinary images

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["www.doi.gov", "storage.googleapis.com", "images.unsplash.com"],
+    domains: [
+      "www.doi.gov",
+      "storage.googleapis.com",
+      "images.unsplash.com",
+      "res.cloudinary.com",
+    ],
   },
 };
 
