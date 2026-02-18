6e68a6888df916a69e72cbab762b400e2ac2f3e1
unw10181 <uchennanwilliams@gmail.com>
2026-02-18 01:31:38 -0500

    allow image display

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ["www.doi.gov", "storage.googleapis.com"],
-    },
+  images: {
+    domains: ["www.doi.gov", "storage.googleapis.com", "images.unsplash.com"],
+  },
 };
 
 export default nextConfig;
